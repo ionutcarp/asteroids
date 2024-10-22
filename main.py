dfaef07e2eea5aa67042ec4685fbb6fec7f657c6
ionutcarp <carpionut@gmail.com>
2024-10-22 17:02:49 +0300

    Ch: 2, L:6 Rotate

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import pygame
 from constants import *
 from player import *
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         # pygame.Surface.fill(screen, "blue")
         screen.fill("blue")
         player.draw(screen)
